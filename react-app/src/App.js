7fed0ab75438f23b798fff47a88c60d173fc7c91
Eugene Ilin <eugenepianist@gmail.com>
2020-09-03 18:27:33 -0400

    created react-app

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2 class='section-title'>Section Title</h2>
+      <div className='bottom-line'></div>
+    </div>
+  );
+}
+
+export default App;
